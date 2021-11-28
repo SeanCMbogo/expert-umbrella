f7c04d2d0d57c4a8ca723b9625bd937399cc4072
Sean C M <Sean.C.Mbogo@gmail.com>
2021-11-27 20:19:06 -0500

    Using time for rudimentary benchmarking

--- echo/echo4.go
+++ echo/echo4.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
+	start := time.Now()
 	fmt.Println(strings.Join(os.Args, " "))
+	fmt.Printf("%.8fs elaspsed \n", time.Since(start).Seconds())
 }
